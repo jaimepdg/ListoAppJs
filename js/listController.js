f26e398f2111ef37165f83049b752940d8b26b32
Jaime Pardo <78267768+jaimepdg@users.noreply.github.com>
2023-11-04 01:56:23 +0100

    cositas de la lista: botones y lista dinamica

--- js/listController.js
+++ js/listController.js
@@ -4,4 +4,28 @@ app.controller('listController', function($scope, myService) {
     myService.setVariable('Lista');
     $scope.title = myService.getVariable()
     
+
+    $scope.showNewItem = false;
+$scope.listItems=[]
+$scope.addNewItem = function() {
+  $scope.showNewItem = true;
+  $scope.showAddBtn = false
+};
+
+$scope.showAddBtn = true
+
+$scope.saveNewItem = function() {
+    $scope.listItems.push($scope.newItemText)
+    $scope.newItemText = '';
+  $scope.showNewItem = false;
+  $scope.showAddBtn = true
+  
+};
+
+$scope.cancelNewItem = function() {
+  $scope.newItemText = '';
+  $scope.showNewItem = false;
+  $scope.showAddBtn = true
+};
+
 });
